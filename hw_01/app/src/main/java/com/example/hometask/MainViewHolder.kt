d55d62859b7d7e22cf33758c0629a39ba5407adb
Binar-code <yardemyd@gmail.com>
2024-10-16 22:26:00 +0300

    Adaptive rotation added

--- hw_01/app/src/main/java/com/example/hometask/MainViewHolder.kt
+++ hw_01/app/src/main/java/com/example/hometask/MainViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.hometask
 
+import android.graphics.Color
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -7,9 +8,14 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainViewHolder(view: View): RecyclerView.ViewHolder(view) {
     private val text: TextView = view.findViewById<TextView>(R.id.text_1)
-    val img = view.findViewById<ImageView>(R.id.image_view)
+    private val img = view.findViewById<ImageView>(R.id.image_view)
 
     fun bind(number: Int) {
         text.text = "$number"
+
+        if (number % 2 == 0)
+            img.setBackgroundColor(Color.RED)
+        else
+            img.setBackgroundColor(Color.BLUE)
     }
 }
