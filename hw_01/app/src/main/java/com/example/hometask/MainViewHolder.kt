d75168a39671f3f99c021c44d756e5c8d6377aea
Binar-code <yardemyd@gmail.com>
2024-10-16 21:58:28 +0300

    Rotate logic added

--- hw_01/app/src/main/java/com/example/hometask/MainViewHolder.kt
+++ hw_01/app/src/main/java/com/example/hometask/MainViewHolder.kt
@@ -1,8 +1,15 @@
 package com.example.hometask
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class MainViewHolder(view: View): RecyclerView.ViewHolder(view) {
-    // TODO
+    private val text: TextView = view.findViewById<TextView>(R.id.text_1)
+    val img = view.findViewById<ImageView>(R.id.image_view)
+
+    fun bind(number: Int) {
+        text.text = "$number"
+    }
 }
